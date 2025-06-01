5ad1abbe4729ccd1e67ddfdaf8e0427462ec69ee
empresaCoxina <empresacoxina@gmail.com>
2025-05-31 21:39:46 -0600

    Update app.py

--- app.py
+++ app.py
@@ -20,5 +20,23 @@ st.header("🤖 Chatbot de Inscripciones")
 st.markdown("Puedes resolver tus dudas o iniciar tu inscripción aquí:")
 
 # Incrustar el chatbot de Landbot (iframe)
-landbot_url = "https://landbot.online/v3/H-2971594-YFSU7IQBD0ZX8MJV/index.html"
-st.components.v1.iframe(landbot_url, height=600, scrolling=True)
+<script>
+window.addEventListener('mouseover', initLandbot, { once: true });
+window.addEventListener('touchstart', initLandbot, { once: true });
+var myLandbot;
+function initLandbot() {
+  if (!myLandbot) {
+    var s = document.createElement('script');
+    s.type = "module"
+    s.async = true;
+    s.addEventListener('load', function() {
+      var myLandbot = new Landbot.Livechat({
+        configUrl: 'https://storage.googleapis.com/landbot.online/v3/H-2971594-YFSU7IQBD0ZX8MJV/index.json',
+      });
+    });
+    s.src = 'https://cdn.landbot.io/landbot-3/landbot-3.0.0.mjs';
+    var x = document.getElementsByTagName('script')[0];
+    x.parentNode.insertBefore(s, x);
+  }
+}
+</script>
